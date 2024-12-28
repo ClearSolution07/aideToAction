0b935df38e308ad010b6caf9b685a491847bf41d
nayanjyoti1234 <nayanjyotisharma675@gmail.com>
2024-12-29 01:07:43 +0530

    Css and minor Changes

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,73 +1,88 @@
-import React from "react";
-import { Layout, Typography, Avatar, Badge, Dropdown, Menu } from "antd";
-import { BellOutlined, MessageOutlined, DownOutlined } from "@ant-design/icons";
-import { profilePhoto } from "../utils/imageUtils";
+import React, {useEffect, useState} from "react";
+import {Layout, Typography, Avatar, Badge, Dropdown, Menu} from "antd";
+import {BellOutlined, MessageOutlined, DownOutlined} from "@ant-design/icons";
+import {profilePhoto} from "../utils/imageUtils";
 
-const { Title } = Typography;
-const { Header } = Layout;
+const {Title} = Typography;
+const {Header} = Layout;
 
 // Menu for the dropdown
 const menuItems = [
-  {
-    key: "profile",
-    label: "Profile",
-  },
-  {
-    key: "settings",
-    label: "Settings",
-  },
-  {
-    key: "logout",
-    label: "Logout",
-  },
+    {
+        key: "profile",
+        label: "Profile",
+    },
+    {
+        key: "settings",
+        label: "Settings",
+    },
+    {
+        key: "logout",
+        label: "Logout",
+    },
 ];
 
 const HeaderComponent = () => {
-  return (
-    <Header
-      style={{
-        background: "#fff",
-        padding: "0 16px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        height: "110px",
-      }}
-    >
-      <Title>Dashboard</Title>
+    const [is_mobile_width, set_is_mobile_width] = useState(false);
 
-      <div
-        style={{
-          display: "flex",
-          gap: 28,
-          alignContent: "center",
-        }}
-      >
-        <div style={{ position: "relative", top: "5px" }}>
-          <Badge dot color="red" offset={[0, 14]}>
-            <BellOutlined style={{ fontSize: 25 }} />
-          </Badge>
-        </div>
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
 
-        <MessageOutlined style={{ fontSize: 25, color: "#000" }} />
+    useEffect(() => {
+        const width = document.body.clientWidth;
+        if (width < 1024) {
+            set_is_mobile_width(true);
+        } else {
+            set_is_mobile_width(false);
+        }
+    }, []);
 
-        <Dropdown menu={{ items: menuItems }} trigger={["click"]}>
-          <div
+    return (
+        <Header
             style={{
-              display: "flex",
-              alignItems: "center",
-              cursor: "pointer",
-              gap: 8,
+                background: "#fff",
+                padding: "0 16px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                height: "110px",
             }}
-          >
-            <Avatar size="large" src={profilePhoto} alt="User Avatar" />
-            <span style={{ fontWeight: 500 }}>Admirra John</span>
-            <DownOutlined />
-          </div>
-        </Dropdown>
-      </div>
-    </Header>
-  );
+        >
+            {!is_mobile_width ? <Title>Dashboard</Title> : null}
+
+            <div
+                style={{
+                    display: "flex",
+                    gap: 28,
+                    alignContent: "center",
+                }}
+            >
+                {!is_mobile_width ? <div style={{position: "relative", top: "5px"}}>
+                    <Badge dot color="red" offset={[0, 14]}>
+                        <BellOutlined style={{fontSize: 25}}/>
+                    </Badge>
+                </div> : null}
+
+                {!is_mobile_width ? <MessageOutlined style={{fontSize: 25, color: "#000"}}/> : null}
+
+                <Dropdown menu={{items: menuItems}} trigger={["click"]}>
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            cursor: "pointer",
+                            gap: 8,
+                        }}
+                    >
+                        <Avatar size="large" src={profilePhoto} alt="User Avatar"/>
+                        <span style={{fontWeight: 500}}>Admirra John</span>
+                        <DownOutlined/>
+                    </div>
+                </Dropdown>
+            </div>
+        </Header>
+    );
 };
 
 export default HeaderComponent;
