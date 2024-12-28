973ec99940786fc8c5d99b156606874b73804cd4
nayanjyoti1234 <nayanjyotisharma675@gmail.com>
2024-12-29 00:01:32 +0530

    Css Changes

--- src/auth/LogIn.jsx
+++ src/auth/LogIn.jsx
@@ -1,150 +1,142 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import React, {useEffect, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
 import {
-  loginImage,
-  logoImage,
-  rightYellow,
-  leftYellow,
-  googleIcon,
+    loginImage,
+    logoImage,
+    rightYellow,
+    leftYellow,
+    googleIcon,
 } from "../utils/imageUtils";
 
 import "./login.css";
-import { Link } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const LogIn = () => {
-  const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-  return (
-    <div className="overContainer">
-      <div className="login-container">
-        <div className="rightCorner">
-          <img src={rightYellow} alt="rightCorner" />
-        </div>
+    const [showPassword, setShowPassword] = useState(false);
+    const [is_mobile_width, set_is_mobile_width] = useState(false);
+    const [password_val, set_password_val] = useState('');
+    const [email_val, set_email_val] = useState('');
 
-        <div
-          className="login-card"
-          style={{ display: "flex", width: "100vw", flexDirection: "column" }}
-        >
-          <div
-            style={{
-              display: "flex",
-              width: "80vw",
-              justifyContent: "end",
-              position: "absolute",
-            }}
-          >
-            <img src={logoImage} alt="logoSarthi" />
-          </div>
-          <div style={{ display: "flex" }}>
-            <div
-              className="loginImage"
-              style={{
-                width: "60vw",
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              <img src={loginImage} alt="loginImage" />
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
+
+    useEffect(() => {
+        const width = document.body.clientWidth;
+        if (width < 1024) {
+            set_is_mobile_width(true);
+        } else {
+            set_is_mobile_width(false);
+        }
+    }, []);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <div className="login-container">
+            {!is_mobile_width ? <div className="leftSection">
+                <div className="loginImage">
+                    <img src={loginImage} alt="loginImage"/>
+                </div>
+                <div className="leftCorner">
+                    <img src={leftYellow} alt="leftCorner"/>
+                </div>
+            </div> : null}
+            <div className={is_mobile_width ? "overallSection" : "rightSection"}>
+                <div className="logoImage">
+                    <img src={logoImage} alt="logoSarthi"/>
+                </div>
+                <div className="formContainer">
+                    <div className="titleSection">
+                        <div className="loginTitle">Welcome</div>
+                        <span>Please login your account</span>
+                    </div>
+                    <div className="loginForm">
+                        <div className="inputContainer">
+                            <span>Email</span>
+                            <input
+                                type="email"
+                                placeholder="Enter your email Id"
+                                className="login-input"
+                                value={email_val}
+                                onChange={(ev) => {
+                                    set_email_val(ev.target.value)
+                                }}
+                                autoFocus={true}
+                            />
+                        </div>
+                        <div
+                            style={{
+                                marginTop: "28px",
+                                marginBottom: "28px",
+                            }}
+                        >
+                            <div className="inputContainer password">
+                                <span>Password</span>
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder="enter your password"
+                                    className="login-input"
+                                    value={password_val}
+                                    onChange={(ev) => {
+                                        set_password_val(ev?.target?.value);
+                                    }}
+                                />
+                                <span
+                                    className="eye-icon"
+                                    onClick={togglePasswordVisibility}
+                                >
+                                    <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye}/>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "20px",
+                        }}
+                    >
+                        <button className="login-button">Sign in</button>
+                        <div className="loginOptions" onClick={() => {
+                            navigate('/register')
+                        }}>
+                            <span href="#" className="login-link">
+                                Didn't have an Account? &nbsp;
+                                <Link
+                                    to="/register"
+                                    style={{
+                                        color: "blue",
+                                        cursor: "pointer",
+                                        textDecoration: "none",
+                                    }}
+                                >
+                                    Register now
+                                </Link>
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div
-              style={{
-                width: "30vw",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                textAlign: "center",
-              }}
-            >
-              <div>
-                <h2 className="login-title">Welcome Back</h2>
-                <span>Please login your account</span>
-              </div>
+            {
+                !is_mobile_width ? <div className="rightCorner">
+                    <img src={rightYellow} alt="rightCorner"/>
+                </div> : null
+            }
 
-              <div className="login-form">
-                <div className="input-container">
-                  <span>Email</span>
-                  <input
-                    type="email"
-                    placeholder="abc123@gmail.com"
-                    className="login-input"
-                  />
-                </div>
-                <div
-                  style={{
-                    marginTop: "28px",
-                    marginBottom: "28px",
-                    position: "relative",
-                  }}
-                >
-                  <div className="input-container psswd">
-                    <span>Password</span>
-                    <input
-                      type={showPassword ? "text" : "password"}
-                      placeholder="enter your password"
-                      className="login-input"
-                    />
-                    <span
-                      className="eye-icon"
-                      onClick={togglePasswordVisibility}
-                    >
-                      <FontAwesomeIcon
-                        icon={showPassword ? faEyeSlash : faEye}
-                      />
-                    </span>
-                  </div>
-
-                  <div
-                    style={{
-                      position: "absolute",
-                      right: "0px",
-                      cursor: "pointer",
-                    }}
-                  >
-                    <span href="#" className="login-link">
-                      Forgot Password ?
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "20px",
-                }}
-              >
-                <button className="login-button">Sign in</button>
-
-                <div className="login-options">
-                  <span href="#" className="login-link">
-                    Didn't have an Account?{" "}
-                    <Link
-                      to="/register"
-                      style={{
-                        color: "blue",
-                        cursor: "pointer",
-                        textDecoration: "none",
-                      }}
-                    >
-                      Register now
-                    </Link>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
+            {!is_mobile_width ? <div className="leftCorner">
+                <img src={leftYellow} alt="leftCorner"/>
+            </div> : null}
         </div>
-        <div className="leftCorner">
-          <img src={leftYellow} alt="lefttCorner" />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default LogIn;
