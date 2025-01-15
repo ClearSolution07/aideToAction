71a9174abbd7e900436c5f57bf3040efad9187f1
harsh23079 <harshmishra6269@gmailclom>
2025-01-16 01:38:57 +0530

    count added

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,9 @@
 export { login, signUp, logout } from "./auth";
-export { getUserDetails, getAllUsers, getAllMembers, getAllPsychologists  } from "./user";
+export {
+  getUserDetails,
+  getAllUsers,
+  getAllMembers,
+  getAllPsychologists,
+} from "./user";
 export { saveChat, getChat } from "./chat";
+export { getNumberOfUser } from "./desktop";
