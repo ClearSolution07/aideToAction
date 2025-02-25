a5f64672528e84644e2e01531ef06ba0794df2fa
harsh23079 <harshmishra6269@gmail.com>
2025-02-25 06:53:00 +0530

    home carousel imporoved

--- src/components/SupportersCarousel.jsx
+++ src/components/SupportersCarousel.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from "react";
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import "../pages/auth/home.css";
 
@@ -44,52 +43,16 @@ const SupportersCarousel = ({ supporters }) => {
                 </h2>
 
                 <div className="carousel-wrapper">
-                    {psychologists && psychologists.length > 0 ? (
-                        <Carousel
-                            responsive={{
-                                superLarge: {
-                                    // Large screens: Display 4 cards
-                                    breakpoint: { max: 4000, min: 1024 },
-                                    items: 4,
-                                },
-                                large: {
-                                    // Medium screens: Display 3 cards
-                                    breakpoint: { max: 1024, min: 768 },
-                                    items: 3,
-                                },
-                                medium: {
-                                    // Small screens: Display 2 cards
-                                    breakpoint: { max: 768, min: 480 },
-                                    items: 2,
-                                },
-                                small: {
-                                    // Very small screens: Display 1 card
-                                    breakpoint: { max: 480, min: 0 },
-                                    items: 1,
-                                },
-                            }}
-                            infinite={true}
-                            autoPlay={true}
-                            keyBoardControl={true}
-                            showDots={true}
-                            arrows={false}
-                        >
-                            {psychologists.map((psychologist) => (
-                                <div
-                                    className="carousel-card"
-                                    key={psychologist.id}
-                                >
-                                    <img
-                                        src={psychologist.image}
-                                        alt={psychologist.name}
-                                        className="psycho-card-image"
-                                    />
-                                </div>
-                            ))}
-                        </Carousel>
-                    ) : (
-                        <p className="no-data">No supporters available</p>
-                    )}
+                    {psychologists.map((psychologist) => (
+                        <div className="carousel-card" key={psychologist.id}>
+                            <img
+                                src={psychologist.image}
+                                alt={psychologist.name}
+                                className="psycho-card-image"
+                            />
+                        </div>
+                    ))}
+                   
                 </div>
             </div>
         </div>
