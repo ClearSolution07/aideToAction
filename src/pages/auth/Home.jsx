81d8940e0588b17666d169d332ada4c81e8664ce
harsh23079 <harshmishra6269@gmail.com>
2025-02-25 09:11:38 +0530

    about is added

--- src/pages/auth/Home.jsx
+++ src/pages/auth/Home.jsx
@@ -40,9 +40,7 @@ const Home = () => {
 
     useEffect(() => {
         const aboutSection = document.querySelector(".about-container");
-        const commitmentSection = document.querySelector(
-            ".commitment-container"
-        );
+      
 
         const observer = new IntersectionObserver(
             (entries) => {
@@ -57,7 +55,6 @@ const Home = () => {
         );
 
         if (aboutSection) observer.observe(aboutSection);
-        if (commitmentSection) observer.observe(commitmentSection);
     }, []);
 
     const showModal = () => {
@@ -125,83 +122,76 @@ const Home = () => {
             </Modal>
 
             <div className="signUp-container">
-                <button onClick={redirectToregister} className="buttons">
-                    SignUp
-                </button>
-                <button className="buttons" onClick={showModal}>
-                    LogIn
-                </button>
                 <div className="left-side">
-                    <h1 className="heading">
-                        Learn with expert anytime anywhere
-                    </h1>
-                    <p className="intro-description">
-                        The vision of Saarthi which is an association of Indian
-                        Careleavers is to advocate for and to empower care
-                        leaving youth transitioning out of child care
-                        institutions (CCI) and other forms of alternative care.
-                    </p>
+                    {/* About Us Section with Animation */}
+                    <div className="about-container" id="aboutSection">
+                        <div className="disclaimer-container">
+                            <h1 className="about-heading">About Us</h1>
+                            <p className="about-text">
+                                <div className="about-description">
+                                    Saarthi is an association of Indian
+                                    careleavers which brings together all the
+                                    careleavers that is those young adults who
+                                    have lived and grown-up in ChildCare
+                                    Institutions. This association is for
+                                    careleavers and by careleavers.
+                                </div>
+
+                                <div className="about-description">
+                                    The association in its initial phase has
+                                    been hosted by Aide et Action in partnership
+                                    with UNICEF and supported by many civil
+                                    society organisations across the country.
+                                    The core group of association comprises of
+                                    representatives from various state level
+                                    networks which are a formal or informal
+                                    group of care leavers at State/District
+                                    level irrespective of their
+                                    religion/class/gender/sexual
+                                    orientation/physical ability/type of their
+                                    institution. You do not have to be a member
+                                    of any state or district network to join
+                                    Saarthi or sign-up here, but you must be a
+                                    careleaver.
+                                </div>
+                                <div className="about-description-bottom">
+                                    With an outreach to more than 4000
+                                    careleavers across India, we welcome you
+                                    with open arms and hearts to join us and be
+                                    a part of this wonderful family. Please
+                                    sign-up to become a member else log-in.
+                                </div>
+                            </p>
+                        </div>
+                        <div className="about-button">
+                            <button
+                                onClick={redirectToregister}
+                                className="buttons"
+                            >
+                                SignUp
+                            </button>
+                            <button className="buttons" onClick={showModal}>
+                                LogIn
+                            </button>
+                        </div>
+                    </div>
                 </div>
                 <div className="right-side">
                     <HomeCarousel homeCarouselData={homeCarouselData} />
                 </div>
             </div>
-
-            {/* About Us Section with Animation */}
-            <div className="about-container" id="aboutSection">
-                <div className="">
-                    <div className="disclaimer-container">
-                        <div className="disclaimer-icon"></div>
-                        <div className="disclaimer-icon-bottom"></div>
-                        <h1 className="about-heading">About Us</h1>
-                        <p className="disclaimer-text">
-                            <div className="about-description">
-                                Saarthi is an association of Indian careleavers
-                                which brings together all the careleavers that
-                                is those young adults who have lived and
-                                grown-up in ChildCare Institutions. This
-                                association is for careleavers and by
-                                careleavers.
-                            </div>
-                            <div className="about-description">
-                                The association in its initial phase has been
-                                hosted by Aide et Action in partnership with
-                                UNICEF and supported by many civil society
-                                organisations across the country. The core group
-                                of association comprises of representatives from
-                                various state level networks which are a formal
-                                or informal group of care leavers at
-                                State/District level irrespective of their
-                                religion/class/gender/sexual
-                                orientation/physical ability/type of their
-                                institution. You do not have to be a member of
-                                any state or district network to join Saarthi or
-                                sign-up here, but you must be a careleaver.
-                            </div>
-                            <div className="about-description-bottom">
-                                With an outreach to more than 4000 careleavers
-                                across India, we welcome you with open arms and
-                                hearts to join us and be a part of this
-                                wonderful family. Please sign-up to become a
-                                member else log-in.
-                            </div>
-                        </p>
-                    </div>
-                </div>
-                <div className="">
-                    <p className="disclaimer-home">
-                        <p style={{ marginBottom: 16 }}>
-                            <strong>*Disclaimer*</strong>{" "}
-                        </p>
-                        Please note, the Saarthi -AICL has no financial, legal
-                        or statutory obligation on either Aide et Action or
-                        UNICEF and vice versa. Aide et Action and UNICEF are and
-                        will not be accountable for any financial, legal, or
-                        statutory repercussions arising out of any activity or
-                        statement done or undertaken by members at their
-                        individual level.{" "}
+            <div className="">
+                <p className="disclaimer-home">
+                    <p style={{ marginBottom: 16 }}>
+                        <strong>*Disclaimer*</strong>{" "}
                     </p>
-                </div>
+                    Please note, the Saarthi -AICL has no financial, legal or
+                    statutory obligation on either Aide et Action or UNICEF and
+                    vice versa. Aide et Action and UNICEF are and will not be
+                    accountable for any financial, legal, or statutory
+                    repercussions arising out of any activity or statement done
+                    or undertaken by members at their individual level.{" "}
+                </p>
             </div>
 
             <div className="purpose-container" id="vissionSection">
