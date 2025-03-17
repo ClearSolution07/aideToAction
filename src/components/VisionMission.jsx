7e187cd07941dd204a42b09f8bfe8cc61d698b3b
AnupamBB <anupamborboruah100@gmail.com>
2025-03-18 00:22:09 +0530

    added some ui changes

--- src/components/VisionMission.jsx
+++ src/components/VisionMission.jsx
@@ -1,13 +1,9 @@
-import React from "react";
 import "./css/visionMission.css";
 
 const VisionMission = () => {
     return (
         <section className="vision-mission-container">
             <h2 className="vision-title">Vision</h2>
-
-           
-
             <p className="vision-description">
                 The Vision of Saarthi, which is an association of Indian
                 Careleavers, is to advocate for and empower care-leaving youth
