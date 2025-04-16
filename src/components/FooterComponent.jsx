aeb49e8727fb52b4db54138628811ab484b85cbc
harsh23079 <harshmishra6269@gmail.com>
2025-04-16 21:54:40 +0530

    footer done

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import logo from "../assets/Logo.png";
+import logo from "../assets/logo2.svg";
+import whatsapp from "../assets/whatsapp.svg";
+
 import {
     FacebookFilled,
     InstagramFilled,
@@ -7,6 +9,7 @@ import {
 } from "@ant-design/icons";
 import twitter from "/img/twitter.png";
 import "./css/footer.css";
+import supporters from "./jsons/supporters.json";
 
 const Footer = ({ isAuthenticated }) => {
     const scrollToSection = (sectionId) => {
@@ -23,120 +26,81 @@ const Footer = ({ isAuthenticated }) => {
     return (
         <footer className="footer">
             <div className="container">
-                <div
-                    className={`footer-grid ${
-                        isAuthenticated ? "center-layout" : ""
-                    }`}
-                >
+                <div className="footer-grid">
                     <div className="brand-section">
                         <div className="brand-header">
                             <img src={logo} alt="Logo" className="brand-logo" />
                         </div>
                     </div>
 
-                    {!isAuthenticated && (
-                        <div className="footer-section">
-                            <h3 className="footer-heading">QUICK LINKS</h3>
-                            <ul className="footer-links">
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="footer-link"
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            scrollToSection("aboutSection");
-                                        }}
-                                    >
-                                        Know More About Us
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="footer-link"
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            scrollToSection("vissionSection");
-                                        }}
-                                    >
-                                        Discover Our Purpose
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="footer-link"
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            scrollToSection(
-                                                "supportersSection"
-                                            );
-                                        }}
-                                    >
-                                        Meet Our Supporters
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    )}
-
                     <div className="footer-section">
-                        <h3 className="footer-heading">CONTACT US</h3>
-
-                        <ul
-                            className="footer-links"
-                            style={{ marginBottom: "2rem" }}
-                        >
-                            <li>
-                                <p className="footer-link">
-                                    aiclsaarthi@gmail.com
-                                </p>
-                            </li>
-                            <li>
-                                <p className="footer-link">+91-8700190013</p>
-                            </li>
-                        </ul>
+                        <div className="footer-heading">
+                            Our Supporters and Donors
+                        </div>
+                        <div className="footer-donor">
+                            <div className="footer-img">
+                                <img
+                                    src={supporters[0].image}
+                                    alt="unicef"
+                                    className="footer-img1"
+                                />
+                            </div>
+                            <div className="footer-img">
+                                <img
+                                    src={supporters[1].image}
+                                    alt="aidAoAction"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="contact-icon">
+                    <div className="contact">
+                        <div className="">MAIL US:</div>
+                        <div>aiclsaarthi@gmail.com</div>
+                        <div className="whatsapp">
+                            <img src={whatsapp} alt="whatsapp" />
+                            <div>+91 8700190013</div>
+                        </div>
                     </div>
 
-                    <div>
-                        <div className="social-icons">
-                            <a
-                                href="https://www.facebook.com/profile.php?id=61562857873961"
-                                className="social-icon"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <FacebookFilled />
-                            </a>
-                            <a
-                                href="https://www.instagram.com/saarthi_aicl"
-                                className="social-icon"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <InstagramFilled />
-                            </a>
-                            <a
-                                href="https://www.linkedin.com/company/saarthi-aicl"
-                                className="social-icon"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <LinkedinFilled />
-                            </a>
-                            <a
-                                href="https://x.com/CareleaversIND"
-                                className="social-icon"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <img
-                                    src={twitter}
-                                    style={{ height: "25px", width: "25px" }}
-                                    alt="Twitter"
-                                />
-                            </a>
-                        </div>
+                    <div className="social-icons">
+                        <a
+                            href="https://www.facebook.com/profile.php?id=61562857873961"
+                            className="social-icon"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FacebookFilled />
+                        </a>
+                        <a
+                            href="https://www.instagram.com/saarthi_aicl"
+                            className="social-icon"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <InstagramFilled />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/company/saarthi-aicl"
+                            className="social-icon"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <LinkedinFilled />
+                        </a>
+                        <a
+                            href="https://x.com/CareleaversIND"
+                            className="social-icon"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                src={twitter}
+                                style={{ height: "25px", width: "25px" }}
+                                alt="Twitter"
+                            />
+                        </a>
                     </div>
                 </div>
 
